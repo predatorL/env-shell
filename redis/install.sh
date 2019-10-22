8f90551da047210938651cc7c67818a76007e5ed
predatorL <peking_opera@163.com>
2019-10-22 23:30:57 +0800

    redis 未完

---
+++ redis/install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# 更新包
+# sudo apt-get update
+# 开始安装
+# sudo apt-get install redis-server
+
+# 提示“是否修改端口”并等待30秒，把用户的输入保存入变量name中
+read -p "是否修改端口 (y/n):" name
+echo -e '\n'
+if [ "$name"='y' ]; then
+    sed -i "s/port 6379/port 6381/g" ./redis/test.conf
+fi
