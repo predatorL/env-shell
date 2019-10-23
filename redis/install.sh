7e252f0ed6799b9451dd038852afa13c77af35ee
predatorL <peking_opera@163.com>
2019-10-23 21:42:43 +0800

    redis

--- redis/install.sh
+++ redis/install.sh
@@ -6,7 +6,10 @@
 
 # 提示“是否修改端口”并等待30秒，把用户的输入保存入变量name中
 read -p "是否修改端口 (y/n):" name
-echo -e '\n'
-if [ "$name"='y' ]; then
-    sed -i "s/port 6379/port 6381/g" ./redis/test.conf
+if [ "$name" = 'y' ]
+    then
+        sed -i "s/port 6379/port 6381/g" ./redis/test.conf
+        echo '修改成功'
+    else
+        echo '放弃修改'
 fi
