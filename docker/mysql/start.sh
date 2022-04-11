61964b7cfabc17f897efb6487d03fcf53597f64f
sishi.lhy <sishi.lhy@alibaba-inc.com>
2022-04-11 20:13:14 +0800

    feat: mysql conf

--- docker/mysql/start.sh
+++ docker/mysql/start.sh
@@ -1,10 +1,15 @@
 
 #! /bin/bash
 # 参数 -p 设置端口，--name 取名 ，-e MYSQL_ROOT_PASSWORD=123456 设置 账号为 root ，密码为 123456 ，-d 表示作为一个守护进程在后台运行
-docker run  \
+docker run -p 3307:3306 \
     --name local_mysql \
+    -v /data/mysql/log:/var/log/mysql \
     -v /data/mysql/data:/var/lib/mysql \
-    -v /data/mysql/my.cnf:/etc/mysql/my.cnf \
-    -p 3307:3306 \
-    -d \
-    mysql
+    -v /data/mysql/conf:/etc/mysql \
+    -e MYSQL_ROOT_PASSWORD=123456 \
+    -d mysql
+    
+# docker run -p 3307:3306 \
+#    --name local_mysql \
+#    -e MYSQL_ROOT_PASSWORD=123456 \
+#    -d mysql
